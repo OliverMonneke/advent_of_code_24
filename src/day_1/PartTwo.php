c9a82c5ba33641ec55ea36c6eaff0d0e32288b7e
Oliver Monneke <o.monneke@googlemail.com>
2024-12-06 11:19:41 +0100

    Add PartTwo class to calculate similarity score
    Implemented the PartTwo class to compute the similarity score between two lists of numbers. This score is derived by multiplying each number in the left list by its occurrence in the right list and summing the results. Updated documentation includes examples and explanation to guide the usage and logic of the computation.

---
+++ src/day_1/PartTwo.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\day_1;
+
+class PartTwo
+{
+    public function findSimilaritySum(): int
+    {
+        $input = file_get_contents(__DIR__ . '/input.txt');
+        $lines = explode("\n", $input);
+        $sum = 0;
+        $left = [];
+        $right = [];
+
+        foreach ($lines as $line) {
+            if (empty($line)) {
+                continue;
+            }
+
+            $lineSplit = explode('   ', $line);
+            $left[] = $lineSplit[0];
+            $right[] = $lineSplit[1];
+        }
+
+        foreach($left as $leftItem) {
+            if (in_array($leftItem, $right)) {
+                # count how often does leftItem appear in the right array
+                $rightCount = array_count_values($right);
+                echo $rightCount[$leftItem] . PHP_EOL;
+                $sum += ($rightCount[$leftItem] * $leftItem);
+            }
+        }
+
+        return $sum;
+    }
+}
+
+$dayTwo = new PartTwo();
+echo $dayTwo->findSimilaritySum();
