63f487fb00227ba1575401435ba265440521afb9
Oliver Monneke <o.monneke@googlemail.com>
2024-12-06 22:19:00 +0100

    Add PHPDoc comments for class properties and methods
    This commit introduces PHPDoc comments to enhance code readability and understanding. It provides type annotations and descriptions for class properties and methods, aiding in better IDE support and code maintenance. These comments are crucial for future developers to grasp the structure and functionality of the code quickly.

--- src/day_1/PartOne.php
+++ src/day_1/PartOne.php
@@ -4,11 +4,26 @@ namespace App\day_1;
 
 class PartOne
 {
+    /**
+     * @var array
+     */
     private array $leftValues = [];
+    /**
+     * @var array
+     */
     private array $rightValues = [];
+    /**
+     * @var string
+     */
     private const string INPUT_FILE_PATH = __DIR__ . '/input.txt';
+    /**
+     * @var int
+     */
     private const int DELIMITER_SPACES = 3;
 
+    /**
+     * @return int
+     */
     public function calculateSumOfDifferences(): int
     {
         $this->loadInputData();
@@ -18,6 +33,9 @@ class PartOne
         return array_sum($this->calculateDifferences());
     }
 
+    /**
+     * @return void
+     */
     private function loadInputData(): void
     {
         $lines = $this->readInputFile();
@@ -31,15 +49,23 @@ class PartOne
         }
     }
 
+    /**
+     * @return array
+     */
     private function readInputFile(): array
     {
         $input = file_get_contents(self::INPUT_FILE_PATH);
         return explode("\n", trim($input));
     }
 
+    /**
+     * @return array
+     */
     private function calculateDifferences(): array
     {
         return array_map(fn($left, $right) => abs($left - $right), $this->leftValues, $this->rightValues);
     }
 }
+
+# $dayOne = new PartOne();
 # echo $dayOne->calculateDifferenceSum();
