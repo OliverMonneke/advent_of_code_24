3e86f35e8894b8b373f884b94b7b093927577035
Oliver Monneke <o.monneke@googlemail.com>
2024-12-06 04:28:03 +0100

    Refactor input parsing and calculation logic
    Moved input parsing to a dedicated method, improving code readability and separation of concerns. Replaced manual iteration with array_map for calculating differences, streamlining the process. Constants were introduced for file paths, enhancing maintainability.

--- src/day_1/PartOne.php
+++ src/day_1/PartOne.php
@@ -7,30 +7,32 @@ class PartOne
     private array $left = [];
     private array $right = [];
 
+    private const string INPUT_FILE_PATH = __DIR__ . '/input.txt';
+
     public function calculateDifferenceSum(): int
     {
-        $input = file_get_contents(__DIR__ . '/input.txt');
+        $this->parseInputFile();
+        sort($this->left);
+        sort($this->right);
+
+        $differences = array_map(fn($left, $right) => abs($left - $right), $this->left, $this->right);
+
+        return array_sum($differences);
+    }
+
+    private function parseInputFile(): void
+    {
+        $input = file_get_contents(self::INPUT_FILE_PATH);
         $lines = explode("\n", $input);
+
         foreach ($lines as $line) {
             if (empty($line)) {
                 continue;
             }
-
-            $lineSplit = explode('   ', $line);
-            $this->left[] = $lineSplit[0];
-            $this->right[] = $lineSplit[1];
+            list($leftValue, $rightValue) = explode('   ', $line);
+            $this->left[] = $leftValue;
+            $this->right[] = $rightValue;
         }
-
-        # sort the arrays
-        sort($this->left);
-        sort($this->right);
-        $sum = 0;
-
-        for ($i = 0; $i < count($this->left); $i++) {
-            $sum += abs($this->left[$i] - $this->right[$i]);
-        }
-
-        return $sum;
     }
 }
 
